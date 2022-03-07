8b75e6ea293273d0a846ea8a20abec7b18583280
streetpitz <ksnaghyuck@gmail.com>
2022-03-07 10:41:25 +0900

    명령문 : 루프 본문

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-X = int(input())
+print('1부터 n까지 정수의 합을 구합니다.')
+n = int(input('n값을 입력하세요.: '))
 
+#준비단계
+sum = 0
+i = 1
+
+#루프 본문
+while i <= n:
+    sum += i
+    i += 1
+
+print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
