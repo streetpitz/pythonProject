1a2cb5df0904af9c0b59364dc2104cbc52371e36
streetpitz <ksnaghyuck@gmail.com>
2022-03-07 11:01:12 +0900

    값 정렬

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-print('1부터 n까지 정수의 합을 구합니다.')
-n = int(input('n값을 입력하세요.: '))
+print('a부터 b까지 정수의 합을 구합니다.')
+a = int(input('정수 a를 입력하세요.: '))
+b = int(input('정수 b를 입력하세요.: '))
 
-#준비단계
+if a > b:
+    a, b = b, a
+
 sum = 0
-i = 1
-
-#루프 본문
-while i <= n:
+for i in range(a, b+1):
     sum += i
-    i += 1
 
-print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
+print(f'{a}부터 {b}까지 정수의 합은 {sum}이다.')
