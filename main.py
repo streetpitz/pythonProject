72543f8cfbe914232221b6881d639554611cb85f
streetpitz <ksnaghyuck@gmail.com>
2022-03-08 10:03:31 +0900

    리스트

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-print('a부터 b까지 정수의 합을 구합니다.')
-a = int(input('정수 a를 입력하세요.: '))
-b = int(input('정수 b를 입력하세요.: '))
+print('학생 그룹 점수의 합계와 평균을 구합니다.')
 
-if a > b:
-    a, b = b, a
+score1 = int(input('1번 학생의 점수를 입력하세요.: '))
+score2 = int(input('2번 학생의 점수를 입력하세요.: '))
+score3 = int(input('3번 학생의 점수를 입력하세요.: '))
+score4 = int(input('4번 학생의 점수를 입력하세요.: '))
+score5 = int(input('5번 학생의 점수를 입력하세요.: '))
 
-sum = 0
-for i in range(a, b+1):
-    sum += i
+total = 0
+total += score1
+total += score2
+total += score3
+total += score4
+total += score5
 
-print(f'{a}부터 {b}까지 정수의 합은 {sum}이다.')
+print(f'합계는 {total}점입니다.')
+print(f'평균은 {total / 5}점입니다.')
